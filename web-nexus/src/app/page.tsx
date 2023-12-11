a7b260e1ebbbd58642a41d25b86f1429c32b6370
victor077 <victor.souza@bandtec.com.br>
2023-12-11 19:50:22 -0300

    nova atts

--- web-nexus/src/app/page.tsx
+++ web-nexus/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/banner'
 import Footer from '@/components/footer'
+import Header from '@/components/header'
 import Informations from '@/components/informations'
 import SecondBanner from '@/components/secondBanner'
 
 export default function Home() {
   return (
     <>
+      <Header />
       <section className="bg-[url('../../public/background-nexus.png')] px-4 h-screen bg-no-repeat bg-cover">
         <Banner />
       </section >
