8ceb72cdad58ff07d618af899399d0e99b70340a
victor077 <victor.souza@bandtec.com.br>
2023-12-11 22:06:58 -0300

    adicionando componente para login

--- web-nexus/src/components/signIn.tsx
+++ web-nexus/src/components/signIn.tsx
@@ -1,8 +1,36 @@
-'use client'
-import React from 'react'
+"use client"
 
-export default function SignIn() {
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "./ui/button";
+
+async function keycloakSessionLogOut() {
+    try {
+        await fetch("/api/auth/logout", { method: "GET" });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export default function SingIn() {
+    const { data: session, status } = useSession();
+
+
+    if (status == "loading") {
+        return <div className="my-3">Loading...</div>;
+    } else if (session) {
+        return (
+            <div className="my-3">
+                Entrar <span className="text-yellow-100">{session.user.email}</span>{" "}
+                <button
+                    className="bg-blue-900 font-bold text-white py-1 px-2 rounded border border-gray-50"
+                    onClick={() => signOut({ callbackUrl: "/" })}>
+                    Sair
+                </button>
+            </div >
+        );
+    }
+
     return (
-        <div></div>
-    )
+        <Button className='hidden md:block px-6' variant="ghost" onClick={() => signIn("keycloak", { redirect: true, callbackUrl: "/home-freelancer" })}>Entrar</Button>
+    );
 }
