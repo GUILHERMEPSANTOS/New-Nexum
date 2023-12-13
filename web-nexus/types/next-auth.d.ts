6f6ad68ed965f272044ce5c2e0c75a2689890aba
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-12-12 22:03:40 -0300

    feat: add new fields to type module

--- web-nexus/types/next-auth.d.ts
+++ web-nexus/types/next-auth.d.ts
@@ -2,18 +2,29 @@ import NextAuth, { DefaultSession } from "next-auth"
 
 declare module 'next-auth' {
     interface Session {
-        accessToken?: string,
+        access_token?: string,
         id_token: string,
+        roles: Array<string>,
         user: {
-            
+
         } & DefaultSession["user"]
     }
 }
 
 declare module 'next-auth/jwt' {
     interface JWT {
-        id_token?: string
-        provider?: string
-        accessToken?: string
+        id_token?: string,
+        provider?: string,
+        access_token?: string,
+        expires_at: number,
+        refresh_token: string,
+        decoded: {
+            sub: string
+            realm_access: RealmAccess
+        },
     }
+}
+
+interface RealmAccess {
+    roles: Array<string>;
 }
