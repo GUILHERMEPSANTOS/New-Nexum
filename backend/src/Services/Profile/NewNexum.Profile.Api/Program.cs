22fa29e424cf5a2e262961b7f1c7743d5e965fa0
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2024-01-04 21:55:26 -0300

    feat: add jwt configs

---
+++ backend/src/Services/Profile/NewNexum.Profile.Api/Program.cs
@@ -0,0 +1,26 @@
+using NewNexum.WebApi.Core.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddAuthConfiguration(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthConfiguration();
+
+app.MapControllers();
+
+app.Run();
