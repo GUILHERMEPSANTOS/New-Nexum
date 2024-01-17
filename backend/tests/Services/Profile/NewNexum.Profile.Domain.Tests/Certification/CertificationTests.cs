670395112b6ff2b2eee7ed1a1b006d7feb27818d
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2024-01-17 20:40:30 -0300

    feat: add new testes

--- backend/tests/Services/Profile/NewNexum.Profile.Domain.Tests/Certification/CertificationTests.cs
+++ backend/tests/Services/Profile/NewNexum.Profile.Domain.Tests/Certification/CertificationTests.cs
@@ -1,4 +1,4 @@
-﻿using NewNexum.Profile.Domain;
+﻿using NewNexum.Core.Communication;
 
 namespace NewNexum.Profile.Domain.CertificationTests
 {
@@ -9,10 +9,36 @@ namespace NewNexum.Profile.Domain.CertificationTests
         public void GivenNewCertificate_WhenAddingNewValidCertificate_ShouldSucceed()
         {
             // Arrange & Act
-            var certificate = Certification.Create(name: "Introdução ao Java", issuingOrganization: "Alura", userId: "uifjigj");
-            
+            var certificate = Certification.Create(name: "Introdução ao Java", issuingOrganization: "Alura", userId: "userId");
+
             //Assert
             Assert.NotNull(certificate);
+            Assert.Contains("Introdução ao Java", certificate.Name);
+            Assert.Contains("Alura", certificate.IssuingOrganization);
+            Assert.Contains("userId", certificate.UserId);
+            Assert.Null(certificate.CredentialURL);
+            Assert.Null(certificate.DateAdded);
+            Assert.Null(certificate.DateOfIssue);
+            Assert.Null(certificate.ExpirationDate);
+        }
+
+        [Fact(DisplayName = "Deve Retornar um novo Certificado")]
+        [Trait("Category", "Certification")]
+        public void GivenOldCertificate_WhenUpdateCertificate_ShouldSucceed()
+        {
+            // Arrange 
+            var certificate = Certification.Create(name: "Introdução ao Java", issuingOrganization: "Alura", userId: "userId");
+            var date = DateTime.UtcNow;
+
+            //Assert &&  Act
+            Assert.Throws<Exception>(
+                () => certificate.Update(
+                        name: null,
+                        issuingOrganization: null,
+                        dateOfIssue: date,
+                        expirationDate: date,
+                        credentialCode: "code",
+                        credentialURL: "Teste dev.io"));
         }
     }
 }
