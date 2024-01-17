61df928d2d3e740f9a417d202ac2373fa3242b5f
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2024-01-17 20:40:05 -0300

    feat: add base entity

---
+++ backend/src/Building Blocks/Core/NewNexum.Core/DomainObjects/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace NewNexum.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
