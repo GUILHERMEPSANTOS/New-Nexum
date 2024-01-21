b37f2dcd12e7a47535ec4219552664ab065a495d
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2024-01-21 16:38:51 -0300

    feat: add messaging abstractions

---
+++ backend/src/Building Blocks/Core/NewNexum.Core/Messaging/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNexum.Core.Messaging
+{
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand: ICommand<TResponse>
+    {
+    }
+}
