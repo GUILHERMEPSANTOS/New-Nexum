dc2a2a04c5c8717ca6f3f871f3fce2c041f4e0b0
GUILHERMEPSANTOS <guilhermepereiradossantos41@outlook.com>
2024-02-08 21:14:34 -0300

    feat: add service installer

---
+++ backend/src/Building Blocks/Services/NewNexum.WebApi.Core/Configurations/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NewNexum.WebApi.Core.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InstallServices(this IServiceCollection services
+              , IConfiguration configuration
+              , Assembly[] assemblies)
+        {
+            var serviceInstallers = assemblies.SelectMany(a => a.DefinedTypes)
+                   .Where(IsAssignableToType<IServiceInstaller>)
+                   .Select(Activator.CreateInstance)
+                   .Cast<IServiceInstaller>();
+
+            foreach (var serviceInstaller in serviceInstallers)
+            {
+                serviceInstaller.Install(services, configuration);
+            }
+
+            return services;
+        }
+
+        private static bool IsAssignableToType<T>(TypeInfo typeInfo)
+        {
+            return typeof(T).IsAssignableFrom(typeInfo) &&
+              !typeInfo.IsInterface &&
+              !typeInfo.IsAbstract;
+        }
+
+    }
+}
