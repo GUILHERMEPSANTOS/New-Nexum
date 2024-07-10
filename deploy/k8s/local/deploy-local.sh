67049b528422fe979b435acda7061869d9cde680
GUILHERMEPSANTOS <guilhermepereiradossantos41@outlook.com>
2024-07-09 21:19:13 -0300

    feat: add config cluster local and multi nodes

--- deploy/k8s/local/deploy-local.sh
+++ deploy/k8s/local/deploy-local.sh
@@ -1,4 +1,5 @@
-kubectl apply -f ./env-config.yml
+kind create cluster --config ./cluster/kind.cluster.yaml
 
+kubectl apply -f profile/profile.env.config.yml
 kubectl apply -f profile/profile.deployment.yml
 kubectl apply -f profile/profile.service.yml
